f3d800e9af0b339e307858f6bb2f34d6079d4ff4
Filippo Squillace <squillace.filippo@gmail.com>
2012-03-29 22:25:37 +0200

    First commit

---
+++ lambda/format.h
@@ -0,0 +1,17 @@
+/*
+ * utils.h
+ *
+ *  Created on: Mar 28, 2012
+ *      Author: Filippo Squillace
+ */
+
+#pragma once
+
+#include <cusp/format.h>
+
+namespace lambda{
+
+struct composite_format : public cusp::unknown_format {};
+
+}
+
